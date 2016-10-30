86a3830279de0deaf3d65d4a8f84ef686175eaee
H. Watanabe <kaityo@users.sourceforge.jp>
2016-10-30 16:59:49 +0900

    adds force_intrin to test_soa.cpp

--- makefile
+++ makefile
@@ -1,23 +1,33 @@
-TARGET= a.out pair.out intrin.out soa.out
+TARGET= aos.out aos_pair.out aos_intrin.out soa.out soa_pair.out soa_intrin.out
+
 all: $(TARGET)
 
-a.out: test.cpp
+aos.out: test.cpp
 	icpc -O3 -xHOST -std=c++11 $< -o $@
+
+aos_pair.out: test.cpp
+	icpc -O3 -xHOST -std=c++11 -DPAIR $< -o $@
+
+aos_intrin.out: test.cpp
+	icpc -O3 -xHOST -std=c++11 -DINTRIN $< -o $@
 
 soa.out: test_soa.cpp
 	icpc -O3 -xHOST -std=c++11 $< -o $@
 
-pair.out: test.cpp
+soa_pair.out: test_soa.cpp
 	icpc -O3 -xHOST -std=c++11 -DPAIR $< -o $@
 
-intrin.out: test.cpp
+soa_intrin.out: test_soa.cpp
 	icpc -O3 -xHOST -std=c++11 -DINTRIN $< -o $@
 
 clean:
 	rm -f $(TARGET)
 
-test:
-	./pair.out > pair.dat
-	./intrin.out > intrin.dat
-	diff pair.dat intrin.dat
+test: aos_pair.out aos_intrin.out soa_pair.out soa_intrin.out
+	./aos_pair.out > aos_pair.dat
+	./aos_intrin.out > aos_intrin.dat
+	diff aos_pair.dat aos_intrin.dat
+	./soa_pair.out > soa_pair.dat
+	./soa_intrin.out > soa_intrin.dat
+	diff soa_pair.dat soa_intrin.dat
 
--- test_soa.cpp
+++ test_soa.cpp
@@ -9,6 +9,10 @@ const int N = 400000;
 const int MAX_PAIRS = 30 * N;
 double L = 50.0;
 const double dt = 0.001;
+const int D = 4;
+enum {X, Y, Z};
+//double q[N][D];
+//double p[N][D];
 double qx[N],qy[N],qz[N];
 double px[N],py[N],pz[N];
 
@@ -23,6 +27,14 @@ int sorted_list[MAX_PAIRS];
 const double CUTOFF_LENGTH = 3.0;
 const double SEARCH_LENGTH = 3.3;
 const double CL2 = CUTOFF_LENGTH * CUTOFF_LENGTH;
+//----------------------------------------------------------------------
+typedef double v4df __attribute__((vector_size(32)));
+//----------------------------------------------------------------------
+void
+print256(v4df r) {
+  double *a = (double*)(&r);
+  printf("%.10f %.10f %.10f %.10f\n", a[0], a[1], a[2], a[3]);
+}
 //----------------------------------------------------------------------
 void
 add_particle(double x, double y, double z) {
@@ -177,6 +189,106 @@ force_sorted(void){
 }
 //----------------------------------------------------------------------
 void
+force_intrin(void){
+  const int pn =particle_number;
+  const v4df vzero = _mm256_set_pd(0, 0, 0, 0);
+  const v4df vcl2 = _mm256_set_pd(CL2, CL2, CL2, CL2);
+  const v4df vc24 = _mm256_set_pd(24 * dt, 24 * dt, 24 * dt, 24 * dt);
+  const v4df vc48 = _mm256_set_pd(48 * dt, 48 * dt, 48 * dt, 48 * dt);
+  for (int i=0; i<pn; i++) {
+    const double qx_key = qx[i];
+    const double qy_key = qy[i];
+    const double qz_key = qz[i];
+    const int np = number_of_partners[i];
+    double pfx = 0;
+    double pfy = 0;
+    double pfz = 0;
+    const int kp = pointer[i];
+    const v4df vqix = _mm256_set_pd(qx[i],qx[i],qx[i],qx[i]);
+    const v4df vqiy = _mm256_set_pd(qy[i],qy[i],qy[i],qy[i]);
+    const v4df vqiz = _mm256_set_pd(qz[i],qz[i],qz[i],qz[i]);
+
+    const v4df vpix = _mm256_set_pd(px[i],px[i],px[i],px[i]);
+    const v4df vpiy = _mm256_set_pd(py[i],py[i],py[i],py[i]);
+    const v4df vpiz = _mm256_set_pd(pz[i],pz[i],pz[i],pz[i]);
+
+    for (int k=0; k<(np/4)*4; k+=4) {
+      const int j_a = sorted_list[kp + k];
+      const int j_b = sorted_list[kp + k + 1];
+      const int j_c = sorted_list[kp + k + 2];
+      const int j_d = sorted_list[kp + k + 3];
+
+      const v4df vqjx = _mm256_set_pd(qx[j_a],qx[j_b],qx[j_c],qx[j_d]);
+      const v4df vqjy = _mm256_set_pd(qy[j_a],qy[j_b],qy[j_c],qy[j_d]);
+      const v4df vqjz = _mm256_set_pd(qz[j_a],qz[j_b],qz[j_c],qz[j_d]);
+
+      const v4df vdx = vqjx - vqix;
+      const v4df vdy = vqjy - vqiy;
+      const v4df vdz = vqjz - vqiz;
+      const v4df vr2 = vdx*vdx+vdy*vdy+vdz*vdz;
+      const v4df vr6 = vr2 * vr2 * vr2;
+      v4df vdf = (vc24 * vr6 - vc48) / (vr6 * vr6 * vr2);
+      const v4df mask = vcl2 - vr2;
+      vdf = _mm256_blendv_pd(vdf, vzero, mask);
+      v4df vdfx = vdf * vdx;
+      v4df vdfy = vdf * vdy;
+      v4df vdfz = vdf * vdz;
+      double *dfx = (double*)(&vdfx);
+      double *dfy = (double*)(&vdfy);
+      double *dfz = (double*)(&vdfz);
+
+      pfx += dfx[3];
+      pfy += dfy[3];
+      pfz += dfz[3];
+
+      px[j_a] -= dfx[3];
+      py[j_a] -= dfy[3];
+      pz[j_a] -= dfz[3];
+
+      pfx += dfx[2];
+      pfy += dfy[2];
+      pfz += dfz[2];
+      px[j_b] -= dfx[2];
+      py[j_b] -= dfy[2];
+      pz[j_b] -= dfz[2];
+
+      pfx += dfx[1];
+      pfy += dfy[1];
+      pfz += dfz[1];
+      px[j_c] -= dfx[1];
+      py[j_c] -= dfy[1];
+      pz[j_c] -= dfz[1];
+
+      pfx += dfx[0];
+      pfy += dfy[0];
+      pfz += dfz[0];
+      px[j_d] -= dfx[0];
+      py[j_d] -= dfy[0];
+      pz[j_d] -= dfz[0];
+    }
+    px[i] += pfx;
+    py[i] += pfy;
+    pz[i] += pfz;
+    for (int k = (np / 4) * 4; k < np; k++) {
+      const int j = sorted_list[kp + k];
+      double dx = qx[j] - qx_key;
+      double dy = qy[j] - qy_key;
+      double dz = qz[j] - qz_key;
+      double r2 = (dx*dx + dy*dy + dz*dz);
+      if (r2 > CL2) continue;
+      double r6 = r2*r2*r2;
+      double df = ((24.0*r6-48.0)/(r6*r6*r2))*dt;
+      px[i] += df*dx;
+      py[i] += df*dy;
+      pz[i] += df*dz;
+      px[j] -= df*dx;
+      py[j] -= df*dy;
+      pz[j] -= df*dz;
+    }
+  }
+}
+//----------------------------------------------------------------------
+void
 force_next(void) {
   const int pn = particle_number;
   for (int i = 0; i < pn; i++) {
@@ -243,13 +355,21 @@ int
 main(void) {
   init();
   makepair();
+#ifdef PAIR
   measure(&force_pair, "pair");
-  measure(&force_sorted, "sorted");
-  measure(&force_next, "sorted_next");
-  /*
   for (int i = 0; i < 10; i++) {
     printf("%.10f %.10f %.10f\n", px[i], py[i], pz[i]);
   }
-  */
+#elif INTRIN
+  measure(&force_intrin, "intrin");
+  for (int i = 0; i < 10; i++) {
+    printf("%.10f %.10f %.10f\n", px[i], py[i], pz[i]);
+  }
+#else
+  measure(&force_pair, "pair");
+  measure(&force_sorted, "sorted");
+  measure(&force_next, "sorted_next");
+  measure(&force_intrin, "intrin");
+#endif
 }
 //----------------------------------------------------------------------

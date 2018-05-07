0cb6bac36baed283c8e38b93ac8fea5a23eae2c6
H. Watanabe <kaityo@users.sourceforge.jp>
2018-05-07 15:57:28 +0900

    インデックス前だし

--- force_soa.cpp
+++ force_soa.cpp
@@ -245,6 +245,10 @@ force_avx2_swp(void) {
     }
 
     for (k = 4; k < (np / 4) * 4; k += 4) {
+      const int j_1_b = sorted_list[kp + k];
+      const int j_2_b = sorted_list[kp + k + 1];
+      const int j_3_b = sorted_list[kp + k + 2];
+      const int j_4_b = sorted_list[kp + k + 3];
       // --- 8< ---
       const v4df mask = vcl2 - vr2;
       vdf = _mm256_blendv_pd(vdf, vzero, mask);
@@ -284,10 +288,16 @@ force_avx2_swp(void) {
       p[Y][j_4] -= dfy[0];
       p[Z][j_4] -= dfz[0];
       // --- 8< ---
+      j_1 = j_1_b;
+      j_2 = j_2_b;
+      j_3 = j_3_b;
+      j_4 = j_4_b;
+      /*
       j_1 = sorted_list[kp + k];
       j_2 = sorted_list[kp + k + 1];
       j_3 = sorted_list[kp + k + 2];
       j_4 = sorted_list[kp + k + 3];
+      */
 
       vqjx = _mm256_set_pd(q[X][j_1], q[X][j_2], q[X][j_3], q[X][j_4]);
       vqjy = _mm256_set_pd(q[Y][j_1], q[Y][j_2], q[Y][j_3], q[Y][j_4]);
